5cdb60117e24f69ca07bffd44fb71f5e5e09cc88
william <masterpkpk@gmail.com>
2020-11-11 10:10:09 -0500

    commit tree 59aefa2e500291e3fb7f38616e8dade9acdd6093

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+    new_array = []
+    if array.length <= 1
+        return array[0]
+    elsif array.length == 2
+        return array.join(' and ')
+    elsif array.length >= 3
+        new_array = array.slice(0..-2)
+        new_array = new_array.join(", ")
+        new_array = new_array + (', and ') + array[-1]
+        return new_array
+
+    end
+
 
 end
